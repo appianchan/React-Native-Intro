7fee5fc1e49ec778632a9fb612f41caf98026e04
Anthony Chan <appianchan@gmail.com>
2020-10-19 10:54:19 -0700

    playing with styles

--- Button_clicker.jsx
+++ Button_clicker.jsx
@@ -7,16 +7,39 @@ export default class App extends React.Component {
   state = {
     count: 0
   }
-  clickme(){
-    this.setState({count: this.state.count + 1})
+  clickme() {
+    this.setState({ count: this.state.count + 1 })
   }
 
-  render(){
-    return(
-      <View>
-        {this.state.count}
-        <Button onPress={this.clickme.bind(this)}/>
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.container2}>
+          <Text style={styles.text}>{this.state.count}</Text>
+        </View>
+
+        <Button style={styles.button} onPress={this.clickme.bind(this)} title='sup' />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  container2: {
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 36,
+    color: 'blue',
+
+    // justifyContent: 'center',
+    // flexDirection: 'row'
+  },
+  button: {
+    color: 'red',
+  }
+})
